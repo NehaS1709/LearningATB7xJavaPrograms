f3066080371ef32779756479d66099636702b958
NehaS1709 <sakale.neha17@gmail.com>
2024-12-23 19:18:49 +0530

    Adding more Files

--- src/ex_01092024/Lab007.java
+++ src/ex_01092024/Lab007.java
@@ -1,7 +1,10 @@
 package ex_01092024;
 
 public class Lab007 {
+    //Program starts after 'main' function
     public static void main(String[] args) {
+        System.out.println("main function"); // executes sout
+        System.out.println("Lab007");// executes
 
     }
-}
+}// ends here or terminates the program here
---
+++ src/ex_02092024/Lab015.java
@@ -0,0 +1,58 @@
+package ex_02092024;
+
+public class Lab015 {
+    public static void main(String[] args) {
+
+        // Structure of variable is as follows:
+        // data_type variable_name = variable_value(literal)
+
+
+        //Data types:
+        //there are 2 types
+        //A) Primitive data types
+          //1. boolean-True or false
+            // eg.
+        boolean married = true;
+        boolean are_you_a_dog = false;
+        System.out.println(married);
+        System.out.println(are_you_a_dog);
+
+
+
+          //2. Numerical
+                //a.Integral
+                    //1. byte: eg-
+        byte b = 127;
+        System.out.println(b);
+                    //2. short: eg-
+        short s = 100;
+        System.out.println(s);
+                    //3. int: eg-
+        int age = 130;
+        System.out.println(age);
+                    //4. long: while using long use L at the end of
+                        // value without space eg-
+        long phone_number = 9876543210L;
+        System.out.println(phone_number);
+                    //5. char-> can only stores characters -> A,a,B,b,etc
+                        // use single inverted commas '' for value for char only
+        char grade = 'A';
+        System.out.println(grade);
+
+                //b.floating point
+                    //1. float: while using long use F or f at the end of
+                        // value without space eg:
+        float pi = 3.14F;
+        System.out.println(pi);
+                    //2. double:eg-
+        double d = 118.6548;
+        System.out.println(d);
+
+
+        //B) Non-Primitive data types
+
+
+
+
+    }
+}
